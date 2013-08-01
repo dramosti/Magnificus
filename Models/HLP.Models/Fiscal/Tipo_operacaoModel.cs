28b3647f92d51ff69ba2f7fe41285b98de609c64
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2013-08-01 09:28:09 -0300

    Correção de forms

--- Models/HLP.Models/Fiscal/Tipo_operacaoModel.cs
+++ Models/HLP.Models/Fiscal/Tipo_operacaoModel.cs
@@ -35,69 +35,69 @@ namespace HLP.Models.Entries.Fiscal
         [ParameterOrder(Order = 13)]
         public byte stEntregaFutura { get; set; }
         [ParameterOrder(Order = 14)]
-        public byte stTpIndustrializacao { get; set; }
+        public byte stTipoIndustrializacao { get; set; }
         [ParameterOrder(Order = 15)]
         public int cCfopNaUf { get; set; }
         [ParameterOrder(Order = 16)]
         public int cCfopOutraUf { get; set; }
         [ParameterOrder(Order = 17)]
-        public int cCfopNcontribuinte { get; set; }
+        public int cCfopNaoContribuinte { get; set; }
         [ParameterOrder(Order = 18)]
         public byte stRelacaoRecebimentoInspecao { get; set; }
         [ParameterOrder(Order = 19)]
         public decimal pInsumoIndustrializacao { get; set; }
         [ParameterOrder(Order = 20)]
-        public decimal pMoIndustrializacao { get; set; }
+        public decimal pMaoObraIndustrializacao { get; set; }
         [ParameterOrder(Order = 21)]
-        public byte stCalcImportacao { get; set; }
+        public byte stCalculaImportacao { get; set; }
         [ParameterOrder(Order = 22)]
-        public byte stCalcCredicmsSs { get; set; }
+        public byte stCalculaCreditoIcmsSuperSimples { get; set; }
         [ParameterOrder(Order = 23)]
-        public byte stCalcCredicmsSsSt { get; set; }
+        public byte stCalculaCreditoIcmsSubstituicaoTributariaSuperSimples { get; set; }
         [ParameterOrder(Order = 24)]
         public byte stReduzBase { get; set; }
         [ParameterOrder(Order = 25)]
         public byte stNaoReduzBase { get; set; }
         [ParameterOrder(Order = 26)]
-        public byte stCalcIcms { get; set; }
+        public byte stCalculaIcms { get; set; }
         [ParameterOrder(Order = 27)]
         public byte stCompoeBaseIcms { get; set; }
         [ParameterOrder(Order = 28)]
-        public byte stCalcIpi { get; set; }
+        public byte stCalculaIpi { get; set; }
         [ParameterOrder(Order = 29)]
         public decimal pIpi { get; set; }
         [ParameterOrder(Order = 30)]
         public byte stCompoeBaseIpi { get; set; }
         [ParameterOrder(Order = 31)]
-        public byte stCalcIcmsSubstTributaria { get; set; }
+        public byte stCalculaIcmsSubstituicaoTributaria { get; set; }
         [ParameterOrder(Order = 32)]
-        public byte stCompoeBaseIcmsSubstTributaria { get; set; }
+        public byte stCompoeBaseIcmsSubstituicaoTributaria { get; set; }
         [ParameterOrder(Order = 33)]
-        public byte stCalcIss { get; set; }
+        public byte stCalculaIss { get; set; }
         [ParameterOrder(Order = 34)]
         public decimal pIss { get; set; }
         [ParameterOrder(Order = 35)]
-        public byte stCalcIrrf { get; set; }
+        public byte stCalculaIrrf { get; set; }
         [ParameterOrder(Order = 36)]
         public decimal pReducaoBaseIrrf { get; set; }
         [ParameterOrder(Order = 37)]
         public decimal pIrrf { get; set; }
         [ParameterOrder(Order = 38)]
-        public byte stCalcCsll { get; set; }
+        public byte stCalculaCsll { get; set; }
         [ParameterOrder(Order = 39)]
-        public byte stCalcInss { get; set; }
+        public byte stCalculaInss { get; set; }
         [ParameterOrder(Order = 40)]
         public decimal pReducaoBaseInss { get; set; }
         [ParameterOrder(Order = 41)]
         public decimal pInss { get; set; }
         [ParameterOrder(Order = 42)]
-        public byte stCalcPisCofins { get; set; }
+        public byte stCalculaPisCofins { get; set; }
         [ParameterOrder(Order = 43)]
         public byte stRegimeTributacaoPisCofins { get; set; }
         [ParameterOrder(Order = 44)]
-        public decimal nCoeficienteSubstTributariaPis { get; set; }
+        public decimal nCoeficienteSubstituicaoTributariaPis { get; set; }
         [ParameterOrder(Order = 45)]
-        public decimal nCoeficienteSubstTributariaCofins { get; set; }
+        public decimal nCoeficienteSubstituicaoTributariaCofins { get; set; }
         [ParameterOrder(Order = 46)]
         public decimal pPis { get; set; }
         [ParameterOrder(Order = 47)]
@@ -109,17 +109,17 @@ namespace HLP.Models.Entries.Fiscal
         [ParameterOrder(Order = 50)]
         public byte stCompoeBaseSubtTribCofins { get; set; }
         [ParameterOrder(Order = 51)]
-        public byte stCalcCustoEntrada { get; set; }
+        public byte stCalculaCustoEntrada { get; set; }
         [ParameterOrder(Order = 52)]
-        public byte stDescontaCredicmsCusto { get; set; }
+        public byte stDescontaCreditoIcmsCusto { get; set; }
         [ParameterOrder(Order = 53)]
-        public byte stDescontaCredsubstCusto { get; set; }
+        public byte stDescontaCreditoSubstituicaoTributariaCusto { get; set; }
         [ParameterOrder(Order = 54)]
-        public byte stDescontaCredipiCusto { get; set; }
+        public byte stDescontaCreditoIpiCusto { get; set; }
         [ParameterOrder(Order = 55)]
-        public byte stDescontaCredpiscofinsCusto { get; set; }
+        public byte stDescontaCreditoPiscofinsCusto { get; set; }
         [ParameterOrder(Order = 56)]
-        public byte stDescontaDifIcmsCusto { get; set; }
+        public byte stDescontaDiferencialIcmsCusto { get; set; }
         [ParameterOrder(Order = 57)]
         public byte stBaixaMaterialOp { get; set; }
         [ParameterOrder(Order = 58)]
@@ -127,11 +127,11 @@ namespace HLP.Models.Entries.Fiscal
         [ParameterOrder(Order = 59)]
         public bool Ativo { get; set; }
         [ParameterOrder(Order = 60)]
-        public byte stExplosaoMatOp { get; set; }
+        public byte? stExplosaoMatOp { get; set; }
         [ParameterOrder(Order = 61)]
-        public byte stEstoqueProdFimOP { get; set; }
+        public byte? stEstoqueProdFimOP { get; set; }
         [ParameterOrder(Order = 62)]
-        public byte stVinculaPedVenda { get; set; }
+        public byte? stVinculaPedidoVenda { get; set; }
         [ParameterOrder(Order = 63)]
         public int idCSTIcms { get; set; }
         [ParameterOrder(Order = 64)]
@@ -144,8 +144,5 @@ namespace HLP.Models.Entries.Fiscal
         public int idCSTPis { get; set; }
         [ParameterOrder(Order = 68)]
         public int idCodigoIcmsPai { get; set; }
-
-
-
     }
 }
