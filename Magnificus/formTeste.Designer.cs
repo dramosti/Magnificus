74a8431d14247cec1c2d4e97dd2a084b4ae8a878
dramosti <diego@hlp.com.br>
2013-08-06 10:25:27 -0300

    frmteste

---
+++ Magnificus/formTeste.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Magnificus
+{
+    partial class formTeste
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formTeste));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.hlP_LabelSeparator1 = new HLP.Comum.Components.HLP_LabelSeparator();
+            this.hlP_TextBox1 = new HLP.Comum.Components.HLP_TextBox();
+            this.hlP_TextBox2 = new HLP.Comum.Components.HLP_TextBox();
+            this.hlP_TextBox3 = new HLP.Comum.Components.HLP_TextBox();
+            this.hlP_ComboBox1 = new HLP.Comum.Components.HLP_ComboBox();
+            this.hlP_NumericUpDown1 = new HLP.Comum.Components.HLP_NumericUpDown();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.Controls.Add(this.hlP_LabelSeparator1);
+            this.flowLayoutPanel1.Controls.Add(this.hlP_TextBox1);
+            this.flowLayoutPanel1.Controls.Add(this.hlP_TextBox2);
+            this.flowLayoutPanel1.Controls.Add(this.hlP_TextBox3);
+            this.flowLayoutPanel1.Controls.Add(this.hlP_ComboBox1);
+            this.flowLayoutPanel1.Controls.Add(this.hlP_NumericUpDown1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(345, 143);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // hlP_LabelSeparator1
+            // 
+            this.hlP_LabelSeparator1._LabelText = "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            this.hlP_LabelSeparator1._TamanhoMaiorLabel = 0;
+            this.hlP_LabelSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_LabelSeparator1.Location = new System.Drawing.Point(3, 3);
+            this.hlP_LabelSeparator1.Name = "hlP_LabelSeparator1";
+            this.hlP_LabelSeparator1.Size = new System.Drawing.Size(183, 19);
+            this.hlP_LabelSeparator1.TabIndex = 5;
+            this.hlP_LabelSeparator1.TabStop = false;
+            // 
+            // hlP_TextBox1
+            // 
+            this.hlP_TextBox1._LabelGroup = this.hlP_LabelSeparator1;
+            this.hlP_TextBox1._LabelText = "1";
+            this.hlP_TextBox1._Multiline = false;
+            this.hlP_TextBox1._Obrigatorio = HLP.Comum.Components.UserControlBase.CampoObrigatorio.NÃO;
+            this.hlP_TextBox1._Password = false;
+            this.hlP_TextBox1._Regex = null;
+            this.hlP_TextBox1._TamanhoComponente = 120;
+            this.hlP_TextBox1._TamanhoMaiorLabel = 63;
+            this.hlP_TextBox1.AllowDrop = true;
+            this.hlP_TextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_TextBox1.Base = null;
+            this.hlP_TextBox1.bConfiguracao = false;
+            this.hlP_TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.hlP_TextBox1.Color = System.Drawing.Color.White;
+            this.hlP_TextBox1.Location = new System.Drawing.Point(53, 28);
+            this.hlP_TextBox1.Margin = new System.Windows.Forms.Padding(53, 3, 15, 3);
+            this.hlP_TextBox1.MaxLength = 32767;
+            this.hlP_TextBox1.Name = "hlP_TextBox1";
+            this.hlP_TextBox1.objConfigComponenteModel = null;
+            this.hlP_TextBox1.ReadOnly = false;
+            this.hlP_TextBox1.Size = new System.Drawing.Size(133, 22);
+            this.hlP_TextBox1.TabIndex = 0;
+            this.hlP_TextBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragDrop);
+            this.hlP_TextBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragEnter);
+            this.hlP_TextBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hlP_TextBox1_MouseDown);
+            // 
+            // hlP_TextBox2
+            // 
+            this.hlP_TextBox2._LabelGroup = this.hlP_LabelSeparator1;
+            this.hlP_TextBox2._LabelText = "2";
+            this.hlP_TextBox2._Multiline = false;
+            this.hlP_TextBox2._Obrigatorio = HLP.Comum.Components.UserControlBase.CampoObrigatorio.NÃO;
+            this.hlP_TextBox2._Password = false;
+            this.hlP_TextBox2._Regex = null;
+            this.hlP_TextBox2._TamanhoComponente = 120;
+            this.hlP_TextBox2._TamanhoMaiorLabel = 63;
+            this.hlP_TextBox2.AllowDrop = true;
+            this.hlP_TextBox2.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_TextBox2.Base = null;
+            this.hlP_TextBox2.bConfiguracao = false;
+            this.hlP_TextBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.hlP_TextBox2.Color = System.Drawing.Color.White;
+            this.hlP_TextBox2.Location = new System.Drawing.Point(53, 56);
+            this.hlP_TextBox2.Margin = new System.Windows.Forms.Padding(53, 3, 15, 3);
+            this.hlP_TextBox2.MaxLength = 32767;
+            this.hlP_TextBox2.Name = "hlP_TextBox2";
+            this.hlP_TextBox2.objConfigComponenteModel = null;
+            this.hlP_TextBox2.ReadOnly = false;
+            this.hlP_TextBox2.Size = new System.Drawing.Size(133, 22);
+            this.hlP_TextBox2.TabIndex = 1;
+            this.hlP_TextBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragDrop);
+            this.hlP_TextBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragEnter);
+            this.hlP_TextBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hlP_TextBox1_MouseDown);
+            // 
+            // hlP_TextBox3
+            // 
+            this.hlP_TextBox3._LabelGroup = this.hlP_LabelSeparator1;
+            this.hlP_TextBox3._LabelText = "3";
+            this.hlP_TextBox3._Multiline = false;
+            this.hlP_TextBox3._Obrigatorio = HLP.Comum.Components.UserControlBase.CampoObrigatorio.NÃO;
+            this.hlP_TextBox3._Password = false;
+            this.hlP_TextBox3._Regex = null;
+            this.hlP_TextBox3._TamanhoComponente = 120;
+            this.hlP_TextBox3._TamanhoMaiorLabel = 63;
+            this.hlP_TextBox3.AllowDrop = true;
+            this.hlP_TextBox3.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_TextBox3.Base = null;
+            this.hlP_TextBox3.bConfiguracao = false;
+            this.hlP_TextBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.hlP_TextBox3.Color = System.Drawing.Color.White;
+            this.hlP_TextBox3.Location = new System.Drawing.Point(53, 84);
+            this.hlP_TextBox3.Margin = new System.Windows.Forms.Padding(53, 3, 15, 3);
+            this.hlP_TextBox3.MaxLength = 32767;
+            this.hlP_TextBox3.Name = "hlP_TextBox3";
+            this.hlP_TextBox3.objConfigComponenteModel = null;
+            this.hlP_TextBox3.ReadOnly = false;
+            this.hlP_TextBox3.Size = new System.Drawing.Size(133, 22);
+            this.hlP_TextBox3.TabIndex = 2;
+            this.hlP_TextBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragDrop);
+            this.hlP_TextBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragEnter);
+            this.hlP_TextBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hlP_TextBox1_MouseDown);
+            // 
+            // hlP_ComboBox1
+            // 
+            this.hlP_ComboBox1._Itens = ((System.Collections.Generic.List<string>)(resources.GetObject("hlP_ComboBox1._Itens")));
+            this.hlP_ComboBox1._LabelGroup = this.hlP_LabelSeparator1;
+            this.hlP_ComboBox1._LabelText = "Combobox";
+            this.hlP_ComboBox1._Obrigatorio = HLP.Comum.Components.UserControlBase.CampoObrigatorio.NÃO;
+            this.hlP_ComboBox1._Regex = null;
+            this.hlP_ComboBox1._situacao = false;
+            this.hlP_ComboBox1._TamanhoComponente = 120;
+            this.hlP_ComboBox1._TamanhoMaiorLabel = 63;
+            this.hlP_ComboBox1.AllowDrop = true;
+            this.hlP_ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_ComboBox1.Base = null;
+            this.hlP_ComboBox1.bConfiguracao = false;
+            this.hlP_ComboBox1.Color = System.Drawing.Color.White;
+            this.hlP_ComboBox1.DataSource = null;
+            this.hlP_ComboBox1.DisplayMember = "DisplayMember";
+            this.hlP_ComboBox1.Location = new System.Drawing.Point(3, 112);
+            this.hlP_ComboBox1.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.hlP_ComboBox1.Name = "hlP_ComboBox1";
+            this.hlP_ComboBox1.objConfigComponenteModel = null;
+            this.hlP_ComboBox1.SelectedIndex = -1;
+            this.hlP_ComboBox1.SelectedValue = 0;
+            this.hlP_ComboBox1.Size = new System.Drawing.Size(183, 21);
+            this.hlP_ComboBox1.TabIndex = 3;
+            this.hlP_ComboBox1.ValueMember = "ValueMember";
+            this.hlP_ComboBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragDrop);
+            this.hlP_ComboBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragEnter);
+            this.hlP_ComboBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hlP_TextBox1_MouseDown);
+            // 
+            // hlP_NumericUpDown1
+            // 
+            this.hlP_NumericUpDown1._LabelGroup = this.hlP_LabelSeparator1;
+            this.hlP_NumericUpDown1._LabelText = "Numeric";
+            this.hlP_NumericUpDown1._Obrigatorio = HLP.Comum.Components.UserControlBase.CampoObrigatorio.NÃO;
+            this.hlP_NumericUpDown1._Regex = null;
+            this.hlP_NumericUpDown1._TamanhoComponente = 120;
+            this.hlP_NumericUpDown1._TamanhoMaiorLabel = 80;
+            this.hlP_NumericUpDown1.AllowDrop = true;
+            this.hlP_NumericUpDown1.BackColor = System.Drawing.Color.Transparent;
+            this.hlP_NumericUpDown1.Base = null;
+            this.hlP_NumericUpDown1.bConfiguracao = false;
+            this.hlP_NumericUpDown1.Color = System.Drawing.Color.White;
+            this.hlP_NumericUpDown1.DecimalPlaces = 2;
+            this.hlP_NumericUpDown1.Location = new System.Drawing.Point(235, 3);
+            this.hlP_NumericUpDown1.Margin = new System.Windows.Forms.Padding(34, 3, 15, 3);
+            this.hlP_NumericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.hlP_NumericUpDown1.Minimum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.hlP_NumericUpDown1.Name = "hlP_NumericUpDown1";
+            this.hlP_NumericUpDown1.objConfigComponenteModel = null;
+            this.hlP_NumericUpDown1.ReadOnly = false;
+            this.hlP_NumericUpDown1.Size = new System.Drawing.Size(169, 22);
+            this.hlP_NumericUpDown1.TabIndex = 4;
+            this.hlP_NumericUpDown1.Value = new decimal(new int[] {
+            0,
+            0,
+            0,
+            131072});
+            this.hlP_NumericUpDown1.ValueInt = 0;
+            this.hlP_NumericUpDown1.DragDrop += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragDrop);
+            this.hlP_NumericUpDown1.DragEnter += new System.Windows.Forms.DragEventHandler(this.hlP_TextBox1_DragEnter);
+            this.hlP_NumericUpDown1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hlP_TextBox1_MouseDown);
+            // 
+            // formTeste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 143);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "formTeste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formTeste";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private HLP.Comum.Components.HLP_TextBox hlP_TextBox1;
+        private HLP.Comum.Components.HLP_TextBox hlP_TextBox2;
+        private HLP.Comum.Components.HLP_TextBox hlP_TextBox3;
+        private HLP.Comum.Components.HLP_ComboBox hlP_ComboBox1;
+        private HLP.Comum.Components.HLP_NumericUpDown hlP_NumericUpDown1;
+        private HLP.Comum.Components.HLP_LabelSeparator hlP_LabelSeparator1;
+    }
+}
